c34ed4296d5755052a8e38a1af3960205d6dbd37
raijinswordigo <mangophonkenjoyer618@gmail.com>
2026-08-02 22:49:00 +0530

    naw

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
 
 Program.Wait(1);
-Game.ShowNotification("ayo chill");
+Game.ShowNotification("nightmare go fuck yourself");
+
+for i = 1, 100 do
+    local kin = Scene.CreateObject("dragonkin_melee");
+    kin:setAlwaysActive(true);
+    kin:setPosition(Scene.Find("hero"):position());
+end
