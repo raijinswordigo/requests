09ae152acaef1840f8c742a597fc73b031819111
raijinswordigo <mangophonkenjoyer618@gmail.com>
2026-08-02 23:07:30 +0530

    .

--- main.lua
+++ main.lua
@@ -10,5 +10,5 @@ ss:setAlwaysActive(true);
 while true do
     Program.Wait(0.01);
     local dir = Entity.GetFacingDirection(hero);
-    ss:setPosition(hero:position() + Vector3.New(dir*200, 0, 0))
+    ss:setVelocity(hero:position() + Vector3.New(dir*200, 0, 0));
 end
