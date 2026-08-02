645f99bd777d269b031eef116bcf41fd843fa933
raijinswordigo <mangophonkenjoyer618@gmail.com>
2026-08-02 23:03:56 +0530

    .

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
 
 Program.Wait(1);
-Game.ShowNotification("i lied bro");
-Character.SetNumCoins(0);
+local hero = Scene.Find("hero");
+
+Game.ShowNotification("follow the ss like a good boyy");
+
+local ss = Scene.CreateObject("sshard_red");
+ss:setAlwaysActive(true);
+
+while true do
+    Program.Wait(0.01);
+    local dir = Entity.GetFacingDirection(hero);
+    ss:setPosition(hero:position() + Vector3.New(dir*200, 0, 0))
+end
