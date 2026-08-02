f3e15ae4b937aa45e13117eb5322f072c8605a8b
raijinswordigo <mangophonkenjoyer618@gmail.com>
2026-08-02 23:59:29 +0530

    .

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ Program.Wait(1);
 local raw = "https://pastebin.com/raw/R0CZPdQP";
 local req = https.request(raw);
 
-while not req:isDone() then
+while not req:isDone() do
     Program.Wait(0.01);
 end
 
